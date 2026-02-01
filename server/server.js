cbc1df9df0798db3e880702814f936404b9041de
sambangiadharsh <sambangiadharsh1729@gmail.com>
2026-02-01 17:59:09 +0530

    update 2

--- server/server.js
+++ server/server.js
@@ -1,26 +1,31 @@
-import 'dotenv/config' 
+
+import 'dotenv/config'
 import express from "express"
 import cors from "cors"
+import { clerkMiddleware, requireAuth } from '@clerk/express'
 
-import { clerkMiddleware,requireAuth } from '@clerk/express'
 import aiRouter from "./routes/aiRoutes.js"
 import userRouter from './routes/userRoutes.js'
 
-const app=express()
+const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://ai-saas-frontend-k1as.onrender.com"
+  ],
+  credentials: true
+}))
+
 app.use(express.json())
 app.use(clerkMiddleware())
 
-app.get('/',(req,res)=>res.send('server is live'))
-app.use(requireAuth())
-app.use('/api/ai',aiRouter);
-app.use('/api/user',userRouter);
+app.get('/', (req, res) => res.send('server is live'))
 
-const PORT=process.env.PORT || 3000;
+app.use('/api/ai', requireAuth(), aiRouter)
+app.use('/api/user', requireAuth(), userRouter)
 
-app.listen(PORT,()=>{
-    console.log('server is running');
-    
-
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log('server is running')
 })
